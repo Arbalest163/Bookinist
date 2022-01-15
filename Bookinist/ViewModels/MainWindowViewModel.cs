135f1adb84df05cc06d46ec2700da2ee368a121c
Arbalest163 <evil_xacker@mail.ru>
2022-01-15 20:43:14 +0400

    Модель главного окна и локатор моделей-представления

---
+++ Bookinist/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using MathCore.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookinist.ViewModels
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        #region Title : string - Заголовок
+
+        private string _Title = "Главное окно программы";
+        /// <summary>
+        /// Заголовок
+        /// </summary>
+        public string Title { get => _Title; set => Set(ref _Title, value); }
+
+        #endregion
+    }
+}
